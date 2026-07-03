8db943099b64fca0a6d1d1f8cfd5ef665774e6ef
Rahul Pundir <rahul4uk.pundir@gmail.com>
2026-07-03 12:53:18 +0530

    Add examples for CommonJS, ES Modules, and Cached Module in Node.js

---
+++ Examples/Section2/CachedDemo.js
@@ -0,0 +1,4 @@
+
+console.log("CachedDemo.js example file is calling");
+export const add = (a, b) => a+b;
+export const multiply = (a, b) => a*b;
---
+++ Examples/Section2/app.js
@@ -0,0 +1,29 @@
+// CommonJsModuleDemo.js example
+
+// var moduleDemo = require('./CommonJsModuleDemo');
+// const result = moduleDemo.add(5, 10);
+// const product = moduleDemo.multiply(5, 10);
+// const quotient = moduleDemo.divide(5, 10);
+// const difference = moduleDemo.subtract(5, 10);
+// console.log(`The result of addition is: ${result}`);
+// console.log(`The result of multiplication is: ${product}`);
+// console.log(`The result of division is: ${quotient}`);
+// console.log(`The result of subtraction is: ${difference}`);
+
+
+// ESModuleDemo.js example
+// import {add, multiply} from './ESModuleDemo.js';
+// const result = add(5, 10);
+// console.log(`The result of addition is: ${result}`);
+
+// CachedDemo.js example
+import {add} from './CachedDemo.js';
+import {multiply } from './CachedDemo.js';
+const result = add(5, 10);
+console.log(`The result of addition is: ${result}`);
+const product = multiply(5, 10);
+console.log(`The result of multiplication is: ${product}`);
+
+
+
+
